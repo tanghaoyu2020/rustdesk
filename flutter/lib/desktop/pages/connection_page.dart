c7e36a4da79e48d29d90a051311b2ace0e09495d
tanghaoyu2020 <253909067@qq.com>
2025-12-15 21:28:10 +0800

    3

--- flutter/lib/desktop/pages/connection_page.dart
+++ flutter/lib/desktop/pages/connection_page.dart
@@ -94,7 +94,7 @@ class _OnlineStatusWidgetState extends State<OnlineStatusWidget> {
                       children: [
                         Flexible(
                           child: Text(
-                            translate('setup_server_tip'),
+                            translate(''),
                             style: TextStyle(
                                 decoration: TextDecoration.underline,
                                 fontSize: em),
